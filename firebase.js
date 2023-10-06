f4f239bb0f97bdaeefe925d3fc9fd6f9e8418831
Volkan Akyel <volkan@uikielle.com>
2023-10-06 13:23:03 +0200

    invoice-builder|#Vite migration 🔁 lab- First steps to migrate from Vue-CLI to Vite

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_INVOICE_API_KEY,
-  authDomain: process.env.VUE_APP_INVOICE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_INVOICE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_INVOICE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_INVOICE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_INVOICE_APP_ID,
-  measurementId: process.env.VUE_APP_INVOICE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VUE_APP_INVOICE_API_KEY,
+  authDomain: import.meta.env.VUE_APP_INVOICE_AUTH_DOMAIN,
+  projectId: import.meta.env.VUE_APP_INVOICE_PROJECT_ID,
+  storageBucket: import.meta.env.VUE_APP_INVOICE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VUE_APP_INVOICE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VUE_APP_INVOICE_APP_ID,
+  measurementId: import.meta.env.VUE_APP_INVOICE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
